d4be0448c397adcf9664109ba54f3b05cb45f5d6
Ricardo Espinoza <rcespinoza2@gmail.com>
2019-05-27 18:08:01 +0200

    Add models

---
+++ db/migrate/20190527160320_create_reviews.rb
@@ -0,0 +1,13 @@
+class CreateReviews < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reviews do |t|
+      t.string :content
+      t.string :rating
+      t.date :date
+      t.references :dish, foreign_key: true
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
