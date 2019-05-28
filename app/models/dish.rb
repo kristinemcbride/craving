40a241b5456f79fc10595c5ad81c16d7377f6664
Ricardo Espinoza <rcespinoza2@gmail.com>
2019-05-28 18:29:45 +0200

    Add dishes search by restaurant address

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -4,4 +4,7 @@ class Dish < ApplicationRecord
   has_many :favorites
 
   mount_uploader :photo, PhotoUploader
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
