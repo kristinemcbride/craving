6311d430c0c3fc442bcbca2d1d77515b8d5e0ee0
Meder Ashiraliev <mikeasher777@gmail.com>
2019-05-28 14:50:27 +0200

    controller fixing

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,7 +1,37 @@
 class DishesController < ApplicationController
+  before_action :set_restaurant, only: [:search]
+
   def show
+    @dish = Dish.find(params[:id])
+  end
+
+  def new
+    @dish = Dish.new
+  end
+
+  def create
+    @dish = Dish.new(strong_dish_params)
+    @dish.user = current_user
+    if @dish.save
+      redirect_to dish_path(@dish)
+    else
+      render :new
+    end
   end
 
   def search
+   raise
+    @dish.restaurant = @restaurant
+    @results = Dish.where("address = ?", params[:address])
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def strong_dish_params
+    params.require(:dish).permit(:name, :photo, :price, :rating, :category, :restaurant_id, :user_id)
   end
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,24 @@
 class RestaurantsController < ApplicationController
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(strong_restaurant_params)
+    if @restaurant.save
+      redirect_to dish_path(@dish)
+    else
+      render :new
+    end
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+
+  def strong_restaurant_params
+    params.require(:restaurant).permit(:name, :address, :average_rating, :price_range, :delivery_url, :booking_url, :longitude, :latitude, :photo)
   end
 end
