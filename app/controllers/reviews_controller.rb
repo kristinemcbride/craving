01dda9ba1f6dd1b4bd32c3d5bb88349ebd0550d9
Meder Ashiraliev <mikeasher777@gmail.com>
2019-05-29 10:08:24 +0200

    test

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,16 @@
 class ReviewsController < ApplicationController
   def new
+    # @review = Review.new
   end
 
   def create
+    # @review = Review.new(review_params)
+    # @restaurant = Restaurant.find(params[:restaurant_id])
+    # @review.restaurant = @restaurant
+    # if @review.save
+    #   redirect_to restaurant_path(@restaurant)
+    # else
+    #   render :new
+    # end
   end
 end
