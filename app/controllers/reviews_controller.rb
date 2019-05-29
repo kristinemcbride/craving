82e5fcc4788465e116dd843ad063f7a726c957b2
Meder Ashiraliev <mikeasher777@gmail.com>
2019-05-29 13:07:11 +0200

    1

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,22 @@
 class ReviewsController < ApplicationController
   def new
-    # @review = Review.new
+    @review = Review.new
   end
 
   def create
-    # @review = Review.new(review_params)
-    # @restaurant = Restaurant.find(params[:restaurant_id])
-    # @review.restaurant = @restaurant
-    # if @review.save
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
     #   redirect_to restaurant_path(@restaurant)
     # else
     #   render :new
     # end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:name, :photo, :price, :rating, :category, :restaurant_id, :user_id)
   end
 end
