0d7643d1960f55a221ab4f0e649122cac4b6c507
Meder Ashiraliev <mikeasher777@gmail.com>
2019-05-30 14:40:30 +0200

    continue with styling

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,7 +1,7 @@
 import places from 'places.js';
 
 const initAutocomplete = () => {
-  const addressInput = document.getElementById('restaurant_address');
+  const addressInput = document.getElementById('address');
   if (addressInput) {
     places({ container: addressInput });
   }
