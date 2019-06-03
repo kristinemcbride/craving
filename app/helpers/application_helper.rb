c723be6868b2711b098d83a18a35d6e3636b1d9e
Kristine McBride <kristinelmcbride@gmail.com>
2019-06-03 10:37:14 +0200

    distance from added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def distance_to_user(user_location, object)
+    # binding.pry
+    # object.distance_to(user_location)
+    distance = object.distance_to(user_location)
+    if distance.nan?
+      'Turn on location'
+    else
+      "#{distance.truncate(2)} km"
+    end
+  end
 end
