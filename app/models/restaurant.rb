8bdfe530b2977822fd24ff36a403bda57b04672c
Kristine McBride <kristinelmcbride@gmail.com>
2019-06-05 09:51:01 +0200

    Refactor restaurant places api call

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -9,6 +9,7 @@ class Restaurant < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_address?
 
   def self.find_or_create(params)
+    binding.pry
     restaurant = self.find_by('name @@ ? AND address @@ ?', params[:name], params[:address])
     if restaurant
       restaurant
